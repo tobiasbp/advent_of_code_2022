57bb0e8627c031cc807b735e7c94492fda750aa7
Tobias Balle-Petersen <tobiasbp@gmail.com>
2022-12-01 13:10:33 +0100

    fix: gnomes -> elves

--- src/day_01.py
+++ src/day_01.py
@@ -8,9 +8,9 @@ from pathlib import Path
 
 def get_calories_list(data_file:Path):
     """
-    Convert a file of calories carried by gnomes to
-    a list of the sum of calories carried by each gnome.
-    Each gnome is separated by an empty line.
+    Convert a file of calories carried by elves to
+    a list of the sum of calories carried by each elf.
+    The data for each elf is separated by an empty line.
     """
 
     # Read data from data file
@@ -19,15 +19,15 @@ def get_calories_list(data_file:Path):
     # Store the calories here
     calories = []
 
-    # Run through the gnome data.
-    for calories_for_gnome in data.split("\n\n"):
-        # Calculate the sum of calories for a gnome
-        calories.append(sum([ int(c) for c in calories_for_gnome.split()]))
+    # Run through the elf data.
+    for calories_for_elf in data.split("\n\n"):
+        # Calculate the sum of calories for an elf
+        calories.append(sum([ int(c) for c in calories_for_elf.split()]))
     
     return calories
 
 c = get_calories_list(Path("data/day_01.data"))
 
 print("Max calories carried:", max(c))
-print("Gnome carying the most calories:", c.index(max(c)))
-print("Sum of calories carried by the three gnomes with most calories:", sum(sorted(c)[-3:]))
+print("Elf carying the most calories:", c.index(max(c)))
+print("Sum of calories carried by the three elfs with most calories:", sum(sorted(c)[-3:]))
